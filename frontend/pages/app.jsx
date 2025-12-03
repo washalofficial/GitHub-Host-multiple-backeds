068b03021f66ef022a68d62563b78e61bcb62d41
washalofficial <washal.official1@gmail.com>
2025-12-04 02:27:14 +0500

    Update app.jsx

--- frontend/pages/app.jsx
+++ frontend/pages/app.jsx
@@ -1,4 +1,5 @@
 import '../styles.css';
-export default function App({ Component, pageProps }) {
+
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
